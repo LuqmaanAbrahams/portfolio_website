5705e74f2c236f804cde8b3f80cf61fbe0e7f29b
luqmaan <luqmaan78600@gmail.com>
2026-08-20 15:51:35 +0200

    Build out hero section on home page with type and leading tokens

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <h1>HELLO WORLD</h1>
-    <h2>TEST WORKS</h2>
+    <section id="hero" class="h-screen">
+      <div class="grid justify-left pr-60 pl-40 py-25 max-h-screen">
+        <div
+          id="kicker"
+          class="flex text-kicker font-400 text-accent-purple-300"
+        >
+          <p
+            class="border-b-1 border-accent-purple-600 w-[40px] translate-y-[-11px] mr-3"
+          ></p>
+          WEBSITE DEVELOPER
+          <p class="pl-2 pr-2 translate-y-[-5px]">.</p>
+          ROODERPOORT, GAUTENG
+        </div>
+        <h1
+          class="font-500 font-display text-hero tracking-hero leading-hero line-clamp-hero pb-[50px]"
+        >
+          Luqmaan <span class="text-accent-purple-600">Abrahams</span>
+        </h1>
+        <p class="text-hero-body text-neutral-300 max-w-[40ch]">
+          You tell me what your site needs to do. I build it, and I'll say so if
+          there's a better way.
+        </p>
+      </div>
+    </section>
+    <section id="selected-work"></section>
   </div>
 </template>
